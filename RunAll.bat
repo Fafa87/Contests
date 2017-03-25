7cd0fbcfc7d7da577b7780c04f4588f8fb90065f
Fafa87 <fafafft@gmail.com>
2017-03-25 01:33:09 +0100

    Easier RunAll by numbers.

--- RunAll.bat
+++ RunAll.bat
@@ -1,13 +1,21 @@
-call RunAsync.bat rocks00
-call RunAsync.bat rocks01
+set name=rocks
 
-call RunAsync.bat rocks02
+call RunAsync.bat %name%00
+call RunAsync.bat %name%01
 
-call RunAsync.bat rocks03
-call RunAsync.bat rocks04
-call RunAsync.bat rocks05
-call RunAsync.bat rocks06
-call RunAsync.bat rocks07
-call RunAsync.bat rocks08
-call RunAsync.bat rocks09
-call RunAsync.bat rocks10
+call RunAsync.bat %name%02
+
+call RunAsync.bat %name%03
+call RunAsync.bat %name%04
+call RunAsync.bat %name%05
+call RunAsync.bat %name%06
+call RunAsync.bat %name%07
+call RunAsync.bat %name%08
+call RunAsync.bat %name%09
+call RunAsync.bat %name%10
+
+REM ALTERNATIVE USAGE: RunAll.bat nazwa_pliku
+REM exe is Deadline.exe		 +
+
+REM for /L %%i in (1,1,10) do if exist input\%10%%i.in  Run.bat %10%%i
+REM for /L %%i in (1,1,10) do if exist input\%11%%i.in  Run.bat %11%%i
