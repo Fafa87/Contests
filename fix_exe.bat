09a0db1036887d550fc03c2a2f42b539b9215419
Wojciech Izykowski <wizykowski@gmail.com>
2017-03-26 11:39:01 +0200

    fix_exe working with VS 2017

--- fix_exe.bat
+++ fix_exe.bat
@@ -1,3 +1,3 @@
 REM %1 is exe path
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
 editbin /stack:100000000 %1
