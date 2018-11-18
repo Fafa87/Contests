f8f0f91aef3739f857557bcce06db07d154ce1c7
qwerty <fafafft@gmail.com>
2018-11-18 01:00:31 +0100

    todosy

--- Solution.cs
+++ Solution.cs
@@ -8,6 +8,34 @@ using System.Diagnostics;
 using Deadline;
 using System.Threading;
 
+// TODO 
+// - mapa z oddzielonych spacjami liczb, a nie tylko stringi => może ustawianie całego rzędu? ale to normalne przypisanie
+// - iterowanie po wszystkich pozycjach
+
+/*
+ * int kolorBud = -1;
+    public void dfs2Bud(HotSpot hot, GridPoint start, int startbasin)
+    {
+        if (budyn[start] == kolorBud)
+            return;
+        budyn[start] = kolorBud;
+        hot.points.Add(start);
+
+        foreach (var move in Moves.All4)
+        {
+            var newpos = move.Move(start);
+            if (mapa.IsInside(newpos))
+            {
+                if (mapa[newpos] == startbasin)
+                {
+                    budyn[start] = kolorBud;
+                    dfs2Bud(hot, newpos, startbasin);
+                }
+            }
+        }
+    }
+ * */
+
 public class Solution : SolutionBase
 {
     public Solution(IClient client, int time = 0)
