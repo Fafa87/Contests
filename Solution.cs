4a788c07ca9782c1bb42ebec82d19001b67a04ef
qwerty <fafafft@gmail.com>
2019-03-22 19:02:09 +0100

    rozgrzebane 7me

--- Solution.cs
+++ Solution.cs
@@ -20,11 +20,13 @@ public class Solution : SolutionBase
     {
     }
 
-    List<Alien> current = new List<Alien>();
+    HashSet<Alien> current = new HashSet<Alien>();
 
     [Serializable]
     public class Alien
     {
+
+        public CType type;
         public int Id;
         public int direction = 0;
         public double speed = 1;
@@ -123,19 +125,41 @@ public class Solution : SolutionBase
 
     }
 
+    public class TowerType
+    {
+        public double Damage;
+        public double Range;
+        public double Cost;
+        public string Name;
+
+    }
+
+    [Serializable]
+    public class CType
+    {
+        public double health;
+        public double speed;
+        public double slow;
+        public double aeoresist;
+        public double loot;
+        public string Name;
+
+    }
+
     public Alien FindClosestAlien(Tower tower, int tick)
     {
         var best = current
             .Where(p=>p.IsStarted(tick))
             .Select(a => (dist: a.Position(tick).Distance(tower.pos), a.Id, a))
+            .Where(a => a.dist <= tower.Range)
             .OrderBy(a => a.dist)
             .ThenBy(a=>a.Item2)
-            .FirstOrDefault(a => a.dist <= tower.Range);
+            .FirstOrDefault();
 
         return best.Item3;
     }
 
-    
+
     public override bool Act()
     {
         var size = Console.ReadLine().ParseList<int>();
@@ -144,40 +168,83 @@ public class Solution : SolutionBase
 
         var commands = Console.ReadLine().Split(' ').ToList();
 
-        var healthspeed = Console.ReadLine().ParseList<double>();
-        var health = healthspeed[0];
-        var speed = healthspeed[1];
-        var loot = healthspeed[2];
 
+
+        var typen = Console.ReadLine().ParseAllTokens<int>();
+        List<CType> typenAlien = new List<CType>();
+        for (int i = 0; i < typen; i++)
+        {
+            var rest = Console.ReadLine().ParseToken(out string name);//<double>();
+            var healthspeed = rest.ParseList<double>();
+            var health = healthspeed[0];
+            var speed = healthspeed[1];
+            var slow = healthspeed[2];
+            var aeoresist = healthspeed[3];
+            var loot = healthspeed[4];
+            typenAlien.Add(new CType() { aeoresist = aeoresist, health = health, Name = name, loot = loot, slow = slow, speed = speed });
+        }
 
         List<Alien> aliens = new List<Alien>();
         var alienn = Console.ReadLine().ParseAllTokens<int>();
-        for (int i =0;i<alienn;i++)
+        for (int i = 0; i < alienn; i++)
         {
-
-            var tiime = Console.ReadLine().ParseAllTokens<int>();
-            var one = new Alien() { direction = 0, speed = speed, StartTime = tiime, Id = i, HP = health, Loot = loot };
+            var rest = Console.ReadLine().ParseToken(out string name);
+            var tiime = rest.ParseAllTokens<int>();
+            var type = typenAlien.First(a => a.Name == name);
+            var one = new Alien() { direction = 0, speed = type.speed, StartTime = tiime, Id = i, HP = type.health, Loot = type.loot, type = type };
             aliens.Add(one);
         }
 
-        current = aliens.DeepClone();
+        var towertypen = Console.ReadLine().ParseAllTokens<int>();
+        var towertypes = new List<TowerType>();
+        for (int i = 0; i < towertypen; i++)
+        {
+            var rest = Console.ReadLine().ParseToken(out string name);//<double>();
+            var data = rest.ParseList<double>();
+            var damage = data[0];
+            var rangea = data[1];
+            var slow = data[2];
+            var aeo = data[3];
+            var cost = data[4];
+            towertypes.Add(new TowerType() { Name = name,  Cost = cost, Damage = damage, Range = rangea });
+        }
+
+        var chosenType = towertypes.FirstOrDefault(a => a.Name == "LASER");
+
+        current = aliens.ToList().DeepClone().ToHashSet();
 
         Alien.CalcTrack(commands, p);
 
-        var damagerange = Console.ReadLine().ParseList<double>();
-        var damage = damagerange[0];
-        var range = damagerange[1];
-        var cost = damagerange[2];
+        
         var goldorg = Console.ReadLine().ParseAllTokens<double>();
         var gold = goldorg;
         var towers = new List<Tower>();
 
         Dictionary<Point, int> close = new Dictionary<Point, int>();
         var trackH = Alien.track.ToHashSet();
+
+        var range = chosenType.Range;// towertypes.Max(t => t.Range);
         foreach (var s in Alien.track)
         {
-            foreach(var move in Moves.All8)
+            for(int i=-(int)range;i<range;i++)
+            for(int i2=-(int)range;i2<range;i2++)
+                {
+                    Point point = new Point(s.X + i, s.Y + i2);
+                    if (point.Distance(s) <= range)
+                    {
+                        if (trackH.Contains(point) == false)
+                        {
+                            close.Increment(point, 1);
+                        }
+
+                    }
+
+                    /*
+                }
+                    foreach (var move in Moves.All8)
             {
+
+
                 var cand = move.Move(s);
                 if (trackH.Contains(cand) == false)
                 {
@@ -191,30 +258,37 @@ public class Solution : SolutionBase
                     {
                         close.Increment(cand2, 1);
                     }
-                }
+                }*/
             }
         }
+
+        Console.Error.WriteLine("Game ready");
 
         while (true)
         {
             towers.Clear();
             foreach (var a in aliens)
-                a.HP = health;
+                a.HP = a.type.health;
             foreach (var a in towers)
             {
                 a.state = State.Seeking;
                 a.lockedOn = null;
             }
-            current = aliens.DeepClone();
+            current = aliens.ToList().DeepClone().ToHashSet();
             gold = goldorg;
 
-            var goodTowers = close.OrderByDescending(a => a.Value).Take(500).Randomize().ToList();
+            var goodTowers = close.OrderByDescending(a => a.Value).ToList();
+            //goodTowers = goodTowers.Take(100).Randomize().Concat(
+            //    goodTowers.Skip(100).Take(100).Randomize().Concat(
+            //        goodTowers.Skip(200).Take(100).Randomize().Concat(
+            //            goodTowers.Skip(300).Take(100).Randomize().Concat(
+            //                goodTowers.Skip(400).Take(100).Randomize())))).ToList();
             int usedTowers = 0;
-            foreach (var t in goodTowers.Take(Math.Min(500, (int)(gold / cost))))
+            foreach (var t in goodTowers.Take(Math.Min(500, (int)(gold / chosenType.Cost))))
             {
-                towers.Add(new Tower() { pos = new Point(t.Key.X, t.Key.Y), Damage = damage, Range = range, Shoot = 0 });
+                towers.Add(new Tower() { pos = new Point(t.Key.X, t.Key.Y), Damage = chosenType.Damage, Range = range, Shoot = 0 });
                 usedTowers++;
-                gold -= cost;
+                gold -= chosenType.Cost;
             }
             goodTowers = goodTowers.Skip(usedTowers).ToList();
 
@@ -233,18 +307,20 @@ public class Solution : SolutionBase
                 if (current.Any(a => a.IsEnd(curtime)))
                 {
                     //Console.Out.WriteLine(curtime);
-                    //Console.Out.WriteLine("LOSS");
+                    Console.Error.Write("LOSS "); Console.Error.WriteLine(current.Count());
                     break;
                 }
                 else
                 {
+                    if (curtime % 100 == 0)
+                        Console.Error.Write(curtime + " ");
                     if (curtime > 0)
                     {
                         foreach (var tower in towers)
                         {
                             if (tower.state == State.Locked)
                             {
-                                if (!tower.InRange(tower.lockedOn.Position(curtime)) || tower.lockedOn.HP <= 0)
+                                if (tower.lockedOn.HP <= 0 || !tower.InRange(tower.lockedOn.Position(curtime)))
                                 {
                                     tower.state = State.Seeking;
                                     tower.lockedOn = null;
@@ -276,12 +352,13 @@ public class Solution : SolutionBase
                         }
 
                         usedTowers = 0;
-                        while(gold >= cost && towers.Count < 500)
+                        while(gold >= chosenType.Cost && towers.Count < 500)
                         {
                             var towernew = goodTowers[usedTowers];
-                            towers.Add(new Tower() { pos = new Point(towernew.Key.X, towernew.Key.Y), Damage = damage, Range = range, Shoot = curtime });
+                            towers.Add(new Tower() { pos = new Point(towernew.Key.X, towernew.Key.Y),
+                                Damage = chosenType.Damage, Range = range, Shoot = curtime });
                             usedTowers++;
-                            gold -= cost;
+                            gold -= chosenType.Cost;
                         }
                         goodTowers = goodTowers.Skip(usedTowers).ToList();
                         usedTowers = 0;
