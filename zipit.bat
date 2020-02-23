19c04f259d0af75c843e874c98893de4a196437f
Wojciech Izykowski <wizykowski@gmail.com>
2020-02-23 14:01:31 +0100

    Do not pack unnecessary files

--- zipit.bat
+++ zipit.bat
@@ -10,6 +10,13 @@ xcopy Properties zip_dir\Properties /i /E
 xcopy DeadlineTest zip_dir\DeadlineTest /i /E
 xcopy Clients zip_dir\Clients /i /E
 
+@REM Remove unnecessary files
+del zip_dir\_Graph\QuickGraphDoc.chm
+del zip_dir\*.exe
+del zip_dir\*.bat
+del zip_dir\*.pdf
+del zip_dir\*.pdb
+
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('zip_dir', 'zip_solution.zip'); }"
 
 rd /S /Q zip_dir
